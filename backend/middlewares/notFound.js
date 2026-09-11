326e35eb7fded4a2cf995fcfaf876abb16d8af2f
aledash3 <cruzd2249@gmail.com>
2026-09-11 10:53:45 -0500

    refactor(cleanup): remove legacy backend and frontend workspace folders after next.js unification

--- backend/middlewares/notFound.js
+++
@@ -1,7 +0,0 @@
-const notFound = (req, res) => {
-    res.status(404).json({
-        mensaje: 'Ruta no encontrada en Judith HairStudio'
-    });
-};
-
-module.exports = notFound;
--- backend/routers/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dashboardController = require('../controllers/dashboardController');
-const asyncHandler = require('../middlewares/asyncHandler');
-
-router.get('/metricas', asyncHandler(dashboardController.obtenerMetricas));
-
-module.exports = router;
